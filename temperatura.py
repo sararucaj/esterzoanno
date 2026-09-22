8ef59c01e0aa26d3d8b655c83104f13d3bdc5b0c
sararucaj <sara.rucaj@campusdavinci.edu.it>
2026-09-22 11:56:22 +0200

    esercizio temperatura

---
+++ temperatura.py
@@ -0,0 +1,11 @@
+"""
+stamparo lo stato di aggregazione dell'acqua data la sua temperatura in input
+
+"""
+temperatura=input("inserisci la tampeeratura dell'acqua in C")
+temperatura=float(temperatura)
+if temperatura<=0:
+    print(solido)
+elif temperatura<=100
+
+            
